9d8929d0914651173fa91cec6a53b43531e15960
subrata6630 <subrata6630@gmail.com>
2024-02-19 15:35:19 +0600

    first commit

---
+++ README.txt
@@ -0,0 +1,48 @@
+=== Bangla Font Hind Siliguri ===
+
+Contributors: Subrata Debnath
+Tags: bangla, Bangla Font, Bengali, Hind Siliguri,Bangladesh Newspaper font
+Requires at least: 3.0.1
+Tested up to: 3.4
+Requires PHP: 5.6
+License: GPLv2 
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Donate link: https://subrata6630.github.io
+Stable tag: trunk
+
+A simple and nice plugin to your website's Bangla text with the "Bangla Font Hind Siliguri"
+
+== Description ==
+
+Enhance the aesthetic appeal of your website's Bangla text with the "Bangla Font Hind Siliguri" plugin. Experience a visually pleasing display of Bangla content, utilizing fonts commonly found in textbooks, ensuring a consistent and beautiful appearance across all devices and browsers.
+
+
+== Installation ==
+
+
+1. Install it from the WordPress plugin repository
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Frequently Asked Questions ==
+
+= How does it work? =
+
+* Simply install and activate the plugin it will try to embed bangla web font automatically.
+* If it doesen’t work then manually edit your theme’s css and change font-family like this:
+
+body {
+font-family: ‘Hind Siliguri’, Arial, sans-serif !important;
+}
+
+
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial Release 
+
+
+
+
+
---
+++ bangla-font-hind-siliguri.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: Bangla font Hind Siliguri
+Plugin URI: http://www.sbtechbd.xyz
+Description: Bangla font Hind Siliguri is an awesome WordPress plugin.Enhance the aesthetic appeal of your website's Bangla text with the "Bangla Font Hind Siliguri" plugin. Experience a visually pleasing display of Bangla content, utilizing fonts commonly found in textbooks, ensuring a consistent and beautiful appearance across all devices and browsers.
+Author: Subrata Debnath
+Version: 1.0.1
+Author URI: http://subrata6630.github.io
+Text Domain: bangla-font-hind-siliguri
+Domain Path: /languages
+License: GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Stable tag: 1.0.1
+*/
+
+
+
+
+defined('ABSPATH') or die('Stop! You can not do this!');
+
+class BanglaFontEmbedder {
+
+    public function __construct() {
+        add_action('wp_head', array($this, 'embedBanglaFont'));
+    }
+
+    public function embedBanglaFont() {
+        $this->enqueueFontStylesheet();
+        $this->addFontStyles();
+    }
+
+    private function enqueueFontStylesheet() {
+        wp_enqueue_style('bangla-font-styles', plugins_url('fonts/font.css', __FILE__), array(), '1.0.0');
+    }
+    
+    private function addFontStyles() {
+        ?>
+        <style>
+            body, article, h1, h2, h3, h4, h5, h6, textarea, input, select, .topbar, .main-menu, .breadcrumb, .copyrights-area, form span.required {
+                font-family: 'Hind Siliguri', Arial, sans-serif !important;
+            }
+        </style>
+        <?php
+    }
+}
+
+// Instantiate the class to initiate the actions
+$banglaFontEmbedder = new BanglaFontEmbedder();
+
